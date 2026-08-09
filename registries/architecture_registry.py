b61f0f47ced3898e0d3abc29eab05a04d3e4afd9
Ahmed Gabr <ahmedoy34@gmail.com>
2026-08-10 00:20:28 +0300

    Added Exception in gitignore for figure images

--- registries/architecture_registry.py
+++ registries/architecture_registry.py
@@ -1,7 +1,6 @@
 from architectures.base_model_architecture import ModelArchitectureBase
 from architectures.simple_cnn import SimpleCNNArchitecture
 
-
 ARCHITECTURE_REGISTRY: dict[str, ModelArchitectureBase] = {
     "simple_cnn": SimpleCNNArchitecture(),
 }
